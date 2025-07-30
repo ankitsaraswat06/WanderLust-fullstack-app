27cf86ee07147bb7773a99eb75480a32eb742aeb
Ankit Saraswat <ankitsaraswat@Ankits-MacBook-Air.local>
2025-07-30 13:59:20 +0530

    changed root route

--- app.js
+++ app.js
@@ -67,9 +67,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/", (req, res)=> {
-//     res.send("Hi i am root");
-// });
+app.get("/", (req, res)=> {
+    res.redirect("/listings");
+});
 
 
 
