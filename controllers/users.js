57997d3b6f5da7d21ca64517634ee692696efe2b
Ankit Saraswat <163433229+ankitsaraswat06@users.noreply.github.com>
2026-02-01 14:19:40 +0530

    new changes

--- controllers/users.js
+++ controllers/users.js
@@ -3,40 +3,55 @@ const User = require("../models/user");
 module.exports.renderSignupForm = (req, res) => {
     res.render("users/signup.ejs");
 };
-module.exports.signup = async(req, res) => {
-    try { 
-        let {username, email, password} = req.body;
-        const newUser = new User({email, username});
+
+module.exports.signup = async (req, res, next) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+
         const registeredUser = await User.register(newUser, password);
-        // console.log(registeredUser);
-        
-        req.login(registeredUser, (err)=> {
-            if(err){
+
+        req.login(registeredUser, (err) => {
+            if (err) {
                 return next(err);
             }
             req.flash("success", "Welcome to Wanderlust");
             res.redirect("/listings");
-        });       
-    } catch(err) {
-        req.flash("error", err.message);
+        });
+
+    } catch (err) {
+        // ✅ Handle duplicate key errors cleanly
+        if (err.code === 11000) {
+            if (err.keyPattern && err.keyPattern.email) {
+                req.flash("error", "An account with this email already exists. Please log in.");
+            } else if (err.keyPattern && err.keyPattern.username) {
+                req.flash("error", "This username is already taken. Please choose another one.");
+            } else {
+                req.flash("error", "User already exists.");
+            }
+        } else {
+            req.flash("error", err.message);
+        }
         res.redirect("/signup");
     }
 };
-module.exports.renderLoginForm = (req, res)=> {
+
+module.exports.renderLoginForm = (req, res) => {
     res.render("users/login.ejs");
 };
 
-module.exports.login = async(req, res)=> {
-        req.flash("success", "Welcome back to Wanderlust! You are logged in")
-        let redirectUrl = res.locals.redirectUrl || "/listings";
-        res.redirect(redirectUrl);
+module.exports.login = async (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust! You are logged in");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 };
-module.exports.logout = (req, res)=> {
-    req.logout((err)=> {
-        if(err) {
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
             return next(err);
         }
-        req.flash("success", "you are logged out!");
+        req.flash("success", "You are logged out!");
         res.redirect("/listings");
-    });  
+    });
 };
